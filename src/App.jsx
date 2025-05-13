10ef9df4764d8510e6f1c9a1e5f8ba49affdd9f2
Ignacio Csaszar <Nachiiitou@XaxiMacBook-Pro-M1.local>
2025-05-13 04:45:59 -0400

    Eliminado HelmetProvider para compatibilidad con React 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+
 import Home from "./pages/Home";
 import "./i18n";
 
 function App() {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/es" />} />
-          <Route path="/es/*" element={<Home lang="es" />} />
-          <Route path="/en/*" element={<Home lang="en" />} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/es" />} />
+        <Route path="/es/*" element={<Home lang="es" />} />
+        <Route path="/en/*" element={<Home lang="en" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
