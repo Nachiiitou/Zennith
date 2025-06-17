75c8e41cd6c567eac19247907d9e349f777917a6
Ignacio Csaszar <Nachiiitou@XaxiMacBook-Pro-M1.local>
2025-06-17 00:33:59 -0400

    a

--- src/blog/como-saber-si-una-web-es-buena-para-google.jsx
+++ src/blog/como-saber-si-una-web-es-buena-para-google.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
+
 import { meta } from "./meta/como-saber-si-una-web-es-buena";
 
 export default function ComoMedirLighthouse() {
